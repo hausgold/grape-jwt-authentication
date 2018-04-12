bad336fcff974167e2ee754f41312a2a64c98f53
Matt Gibson <matt@farmdrop.com>
2018-04-12 11:14:28 +0100

    Failing spec showing that custom content types are not picked up correctly

--- spec/grape/jwt/grape_usage_spec.rb
+++ spec/grape/jwt/grape_usage_spec.rb
@@ -31,6 +31,30 @@ module TestGlobalConfiguration
   end
 end
 
+module TestContentType
+  class API < Grape::API
+    version 'v1', using: :path
+
+    content_type :jsonapi, "application/vnd.api+json"
+    default_format :jsonapi
+    format :jsonapi
+
+
+    include Grape::Jwt::Authentication
+    auth :jwt, malformed: $custom_malformed_auth_handler,
+         failed: $custom_failed_auth_handler,
+         &$custom_authenticator
+
+
+    resource :test do
+      desc 'A simple GET endpoint which is JWT protected.'
+      post do
+        { test: true }
+      end
+    end
+  end
+end
+
 module TestLocalConfiguration
   class API < Grape::API
     version 'v1', using: :path
@@ -51,10 +75,6 @@ module TestLocalConfiguration
 end
 
 RSpec.shared_examples 'api' do
-  let(:malformed_token) { 'no.json.web.token' }
-  let(:invalid_token) { 'eyJ0eXAiOiJKV1QifQ.eyJ0ZXN0IjpmYWxzZX0.' }
-  let(:valid_token) { 'eyJ0eXAiOiJKV1QifQ.eyJ0ZXN0Ijp0cnVlfQ.' }
-
   it 'fails on missing authorization header' do
     get '/v1/test'
     expect(last_response.body).to \
@@ -84,6 +104,10 @@ end
 
 # rubocop:disable RSpec/DescribeClass because we test not a specific class
 RSpec.describe 'Grape usage' do
+  let(:valid_token) { 'eyJ0eXAiOiJKV1QifQ.eyJ0ZXN0Ijp0cnVlfQ.' }
+  let(:malformed_token) { 'no.json.web.token' }
+  let(:invalid_token) { 'eyJ0eXAiOiJKV1QifQ.eyJ0ZXN0IjpmYWxzZX0.' }
+
   include Rack::Test::Methods
 
   before { Grape::Jwt::Authentication.reset_configuration! }
@@ -106,5 +130,20 @@ RSpec.describe 'Grape usage' do
     let(:app) { TestLocalConfiguration::API }
 
     include_examples 'api'
+  end
+
+  context 'with a custom content type' do
+    let(:app) { TestContentType::API }
+
+    it 'succeeds on POST' do
+      header 'Authorization', "Bearer #{valid_token}"
+      header 'CONTENT_TYPE', 'application/vnd.api+json'
+      header 'HTTP_ACCEPT', 'application/vnd.api+json'
+
+      post '/v1/test',
+           params: {},
+           as: :json
+      expect(last_response.body).to be_eql('{"test":true}')
+    end
   end
 end
