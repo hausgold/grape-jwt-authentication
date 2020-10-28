1ebb90e80e9d2573b6ad92e7062f256b4a3ad6b8
Hermann Mayer <hermann.mayer92@gmail.com>
2020-10-28 09:53:31 +0100

    Pinned rspec-expectations to <= 3.9.2.
    Signed-off-by: Hermann Mayer <hermann.mayer92@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,6 +24,11 @@ RSpec.configure do |config|
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
+  # Enable the focus inclusion filter and run all when no filter is set
+  # See: http://bit.ly/2TVkcIh
+  config.filter_run(focus: true)
+  config.run_all_when_everything_filtered = true
+
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
