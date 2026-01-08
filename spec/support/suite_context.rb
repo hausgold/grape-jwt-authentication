eb60d12f31d4bdbeee3827398add6b5e52dff025
Hermann Mayer <hermann.mayer92@gmail.com>
2026-01-08 15:03:13 +0100

    Corrected some Rubocop glitches.
    Signed-off-by: Hermann Mayer <hermann.mayer92@gmail.com>

--- spec/support/suite_context.rb
+++ spec/support/suite_context.rb
@@ -3,6 +3,7 @@
 # Print some information
 #
 # rubocop:disable Rails/Output -- because we want to write to stdout here
+# rubocop:disable RSpec/Output -- ditto
 puts
 puts <<DESC
   -------------- Versions --------------
@@ -13,3 +14,4 @@ puts <<DESC
 DESC
 puts
 # rubocop:enable Rails/Output
+# rubocop:enable RSpec/Output
