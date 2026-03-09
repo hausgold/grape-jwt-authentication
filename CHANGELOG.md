dffd686202019faa43346965c04ac585ac5f6b26
Hermann Mayer <hermann.mayer92@gmail.com>
2026-03-09 06:12:29 +0100

    Corrected some RuboCop issues.
    Signed-off-by: Hermann Mayer <hermann.mayer92@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 ### next
 
-* TODO: Replace this bullet point with an actual description of a change.
+* Corrected some RuboCop issues (#38)
 
 ### 3.10.0 (18 February 2026)
 
--- spec/grape/jwt/grape_usage_spec.rb
+++ spec/grape/jwt/grape_usage_spec.rb
@@ -15,6 +15,7 @@ $custom_failed_auth_handler = proc do |_token, _app|
   [401, {}, ['Go away!']]
 end
 
+# rubocop:disable Style/OneClassPerFile -- because of the various usage specs
 module TestGlobalConfiguration
   class API < Grape::API
     version 'v1', using: :path
@@ -136,6 +137,7 @@ RSpec.shared_examples 'api' do
     end
   end
 end
+# rubocop:enable Style/OneClassPerFile
 
 # rubocop:disable RSpec/DescribeClass -- because we test not a specific class
 RSpec.describe 'Grape usage' do
